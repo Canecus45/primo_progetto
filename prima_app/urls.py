1295290ae4a733c2d5bb932eae591024d9d176d8
Canecus45 <149671604+Canecus45@users.noreply.github.com>
2024-12-04 11:35:10 +0100

    iowjf

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homepage, welcome, lista, chi_siamo
+from .views import homepage, welcome, lista, chi_siamo, variabili, index
 
 app_name = "prima_app"
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("welcome", welcome, name="welcome"),
     path("lista", lista, name="lista"),
     path("chi_siamo", chi_siamo, name="chi_siamo"),
+    path("variabili", variabili, name="variabili"),
+    path("index", index, name="index"),
 ]
--- prima_app/views.py
+++ prima_app/views.py
@@ -17,3 +17,17 @@ def lista(request):
 
 def chi_siamo(request):
     return render(request, "chi_siamo.html")
+
+
+def variabili(request):
+    context = {
+        "var1": "Prima varabile",
+        "var2": "Seconda varabile",
+        "var3": "Terza varabile",
+    }
+
+    return render(request, "variabili.html", context)
+
+
+def index(request):
+    return render(request, "index.html")
