e04af5476d662900f87cf169ca1011d2bd6c3e24
Canecus45 <149671604+Canecus45@users.noreply.github.com>
2024-12-31 10:40:19 +0100

    wfwe

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import es_if, index2
+from .views import index2, es_if, es_if_else_elif
 
 app_name = "seconda_app"
 
 urlpatterns = [
-    path("es_if", es_if, name="es_if"),
     path("index2", index2, name="index2"),
+    path("es_if", es_if, name="es_if"),
+    path("es_if_else_elif", es_if_else_elif, name="es_if_else_elif"),
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -4,9 +4,21 @@ from django.shortcuts import render
 
 
 def es_if(request):
-    context = {"var1": 100, "var2": 200, "var3": 300}
+    context = {
+        "var1": 200,
+        "var2": 200,
+        "var3": 300,
+    }
     return render(request, "es_if.html", context)
 
 
 def index2(request):
     return render(request, "index2.html")
+
+def es_if_else_elif(request):
+    context = {
+        "var1": 100,
+        "var2": 100.0,
+        "var3": 100.50,
+    }
+    return render(request, "es_if_else_elif.html", context)
