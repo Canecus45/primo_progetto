bf30633912261b0634432abc32d6a34ec8106c8c
Canecus45 <149671604+Canecus45@users.noreply.github.com>
2025-02-12 09:10:53 +0100

    asfg

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import home,articoloDetailView
+from .views import home, index, articoloDetailView, listaArticoli, queryBase, giornalistaDetailView
 
-app_name='news'
+app_name = "news"
 
 urlpatterns = [
-    path('',home,name="homewiew"),
-    path('articoli/<int:pk>',articoloDetailView,name="Articolo_detail"),
+    path('', index, name='index'),
+    path('home', home, name='homeview'),
+    path('articoli/<int:pk>', articoloDetailView, name='articolo_detail'),
+    path('articoli', listaArticoli, name='lista_articoli'),
+    path('giornalisti/<int:pk>', giornalistaDetailView, name='giornalista_detail'),
+    #path('giornalisti', listaGiornalisti, name='lista_giornalisti),
+    path('query', queryBase, name='query_base'),
 ]
